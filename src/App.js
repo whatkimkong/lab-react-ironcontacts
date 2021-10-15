2f428b53635755c35d1d05c7aeb3725db59698a8
whatkimkong <whatkimkong@gmail.com>
2021-10-15 18:13:11 +0200

    Attempted Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
+
+const tableStyle = {
+  border: 'none',
+  width: '40%',
+  display: 'flex',
+  flexDirection: 'row',
+  textAlign: 'left',
+  padding: '5px',
+}
 
 function App() {
+
+  state = {
+    first5Contacts: contacts.splice(0, 4),
+  }
+
   return (
-    <div className="App">
+    <><div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>IronContacts</h2>
       </header>
-    </div>
+    </div><div>
+        <table style={tableStyle}>
+          <thead>
+            <tr>
+              <th scope="col">Picture</th>
+              <th scope="col">Name</th>
+              <th scope="col">Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.first5Contacts.map((eachContact) => {
+              return (
+            <tr>
+              <td>{eachContact.name}</td>
+              <td></td>
+              <td>{eachContact.popularity}</td>
+            </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div></>
   );
 }
+
+// ?? the picture how?
+// <img src=`{eachContact.pictureUrl}`/>
+
+
+// why the fragment?
 
 export default App;
