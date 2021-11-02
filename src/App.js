3d31e267db3b04f04ef205d171eb79d40a0ae908
whatkimkong <whatkimkong@gmail.com>
2021-11-02 16:30:53 +0100

    Iteration 4 Attempted - need ID to pass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, {Component} from 'react';
 import contacts from "./contacts.json"; 
-import { Button, Container } from 'react-bootstrap';
 
 /*
 const tableStyle = {
@@ -19,7 +18,7 @@ class App extends Component {
   state = {
     contactsDisplayed: contacts.slice(0, 5),
   }
- 
+
   addRandomContact = () => {
     const randomIndex = Math.floor(Math.random()*contacts.length);
     const randomContact = contacts[randomIndex];
@@ -44,21 +43,29 @@ class App extends Component {
     this.setState({contactsDisplayed: clonedContacts})
   }
 
+  handleDelete = () => {
+    const clonedContacts = [...this.state.contactsDisplayed];
+    this.setState({contactsDisplayed: clonedContacts.slice(0,-1)})
+  }
+
   render() {
   return (
-    <Container className="justify-content-center">
-    
+    <div className="App">
+        <div className="table">
         <h2 >IronContacts</h2>
-        <Button onClick={this.addRandomContact}>Add a New Contact</Button>
-        <Button onClick={this.sortByName}>Sort By Name</Button>
-        <Button onClick={this.sortByPopularity}>Sort By Popularity</Button>
+        <button onClick={this.addRandomContact}>Add a New Contact</button>
+        <button onClick={this.sortByName}>Sort By Name</button>
+        <button onClick={this.sortByPopularity}>Sort By Popularity</button>
         <hr/>
-        <table >
+        </div>
+        <div>
+        <table>
           <thead>
             <tr>
               <th scope="col">Picture</th>
               <th scope="col">Name</th>
               <th scope="col">Popularity</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
@@ -68,12 +75,14 @@ class App extends Component {
               <td key={eachContact}><img alt="contactImage" src={eachContact.pictureUrl} width="40vw"/></td>
               <td key={eachContact.name}>{eachContact.name}</td>
               <td key={eachContact.popularity}>{eachContact.popularity}</td>
+              <td><button onClick={this.handleDelete}>Delete</button></td>
             </tr>
               )
             })}
           </tbody>
         </table>
-    </Container>
+        </div>
+    </div>
   )
   }
 }
